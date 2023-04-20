ef98828f7e7941d8afcb74ca6523eafd4a696cde
Мой Царь <van2015@mail.ru>
2023-04-20 16:34:43 +0300

    profile fixed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,8 +51,8 @@ class AuthController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'phone' => 'required|min:11|unique:users',
-            'address' => 'required',
+            'phone' => $data['phone'],
+            'address' => $data['address'],
         ]);
 
         Auth::login($user);
